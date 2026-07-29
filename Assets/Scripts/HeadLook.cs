d9419097f092332f6526b3576498fecaecc0d342
Bui Khanh Duy <buikhanhduy13082003@gmail.com>
2026-07-29 19:54:02 +0700

    Fixing character head look.

--- Assets/Scripts/HeadLook.cs
+++ Assets/Scripts/HeadLook.cs
@@ -6,18 +6,43 @@ public class HeadLook : MonoBehaviour
     private Transform headObj;
     [SerializeField]
     private Transform targetObj;
+    [SerializeField]
+    private Transform headForward;
+    [SerializeField]
+    private float minAngle;
+    [SerializeField]
+    private float maxAngle;
+
+    private float lookSpeed = 7.0f;
+    private Quaternion defaultRotation;
 
     private Vector3 offsetEuler = new Vector3(0, -90, 0);
+
+    private void Start()
+    {
+        defaultRotation = headObj.rotation;
+    }
 
     private void LateUpdate()
     {
         Vector3 dir = targetObj.position - headObj.position;
-
         if (dir.sqrMagnitude < 0.0001f) return;
 
-        Quaternion lookRotation = Quaternion.LookRotation(dir, Vector3.up);
-        Quaternion offset = Quaternion.Euler(offsetEuler);
+        float angle = Vector3.SignedAngle(dir, headForward.forward, headForward.up);
+        Debug.Log("Angle: " + angle);
 
-        headObj.rotation = lookRotation * Quaternion.Inverse(offset);
+        if (angle < maxAngle && angle > minAngle)
+        {
+            //Quaternion targetRotation = Quaternion.LookRotation(dir, Vector3.up);
+            Quaternion targetRotation = Quaternion.LookRotation(targetObj.position - headObj.position);
+            Quaternion offset = Quaternion.Euler(offsetEuler);
+            defaultRotation = Quaternion.Slerp(defaultRotation, targetRotation, lookSpeed * Time.deltaTime);
+            headObj.rotation = defaultRotation * Quaternion.Inverse(offset);
+        }
+        else
+        {
+            defaultRotation = Quaternion.Slerp(defaultRotation, headForward.rotation, lookSpeed * Time.deltaTime);
+            headObj.rotation = defaultRotation;
+        }
     }
 }
